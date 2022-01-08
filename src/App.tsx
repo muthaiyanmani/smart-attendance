22d69a23eee5359cf573b48160a3969c2d59f70f
Muthaiyan Mani <muthaiyanmani00@gmail.com>
2022-01-08 06:48:04 +0530

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-green-500'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
