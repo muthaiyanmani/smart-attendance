7669fb7c58cfd37bbde4eac0fa77fc07cb9678ad
Muthaiyan Mani <muthaiyanmani00@gmail.com>
2022-04-17 07:27:33 +0530

    Update settings.ts

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL: string = "http://localhost:4000/api";
-export const BASE_URL: string = "https://smaart-attendance.herokuapp.com/api";
+export const BASE_URL: string = "http://localhost:4000/api";
+// export const BASE_URL: string = "https://smaart-attendance.herokuapp.com/api";
